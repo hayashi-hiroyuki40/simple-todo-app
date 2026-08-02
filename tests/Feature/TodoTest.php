4916d5f32587b8d74aabbad77d6aa169e1176136
Hiroyuki Hayashi <ai3672ght1@gmail.com>
2026-08-02 16:38:48 +0900

    feat: TODO一覧画面への検索機能の実装#7

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,8 +15,13 @@ class TodoTest extends TestCase
      */
     public function test_index_page_can_be_accessed(): void
     {
-        $todo1 = Todo::create(['content' => 'test']);
-        $todo2 = Todo::create(['content' => 'test2']);
+        $category = Category::create([
+        'name' => 'テストカテゴリ'
+        ]);
+        $todo1 = Todo::create(['content' => 'test',
+        'category_id' => $category->id,]);
+        $todo2 = Todo::create(['content' => 'test2',
+        'category_id' => $category->id,]);
     
         $response = $this->get('/');
 
@@ -31,7 +37,11 @@ class TodoTest extends TestCase
 
     public function test_store_can_create_a_todo(): void
     {
-    $data=['content'=>'テスト用のタスクです',];
+    $category = Category::create([
+        'name' => 'テストカテゴリ'
+    ]);
+    $data=['content'=>'テスト用のタスクです',
+        'category_id' => $category->id,];
 
     $response= $this->post('/todos',$data);
 
@@ -48,18 +58,24 @@ class TodoTest extends TestCase
 
     public function test_update_can_update_a_todo(): void
     {
+        $category = Category::create([
+        'name' => 'テストカテゴリ'
+    ]);
         $todo=Todo::create([
-            'content'=>'テスト用の更新前のタスクです'
+            'content'=>'テスト用の更新前のタスクです',
+        'category_id' => $category->id,
         ]);
     
         $data=['id'=>$todo->id,
-            'content'=>'テスト用の更新タスクです'];
+            'content'=>'テスト用の更新タスクです',
+        'category_id' => $category->id,];
 
-        $response=$this->put('/todos/update',$data);
+        $response=$this->patch('/todos/update',$data);
 
         $this->assertDatabaseHas('todos',[
             'id'=>$todo->id,
             'content'=>'テスト用の更新タスクです',
+        'category_id' => $category->id,
         ]);
         $response->assertRedirect('/');
 
@@ -70,8 +86,12 @@ class TodoTest extends TestCase
 
     public function test_destroy_can_destroy_a_todo(): void
     {
+        $category = Category::create([
+        'name' => 'テストカテゴリ'
+    ]);
         $todo=Todo::create([
-            'content'=>'テスト用の削除前タスクです']);
+            'content'=>'テスト用の削除前タスクです',
+        'category_id' => $category->id,]);
         
         $response=$this->delete('/todos/delete',['id'=>$todo->id]);
 
@@ -84,5 +104,60 @@ class TodoTest extends TestCase
         $response = $this->get('/');
         $response->assertStatus(200);
         $response->assertDontSee('テスト用の削除前タスクです',false);
+    }
+
+    public function test_can_search_Todo_by_keyword(): void
+    {
+        
+        $category=Category::create(['name'=>'テストカテゴリ']);
+
+        $todo1=Todo::create([
+            'content'=>'ゲームする','category_id'=>$category->id,
+        ]);
+        $todo2=Todo::create([
+            'content'=>'読書する','category_id'=>$category->id
+        ]);
+
+        $response=$this->get('/todos/search?keyword=ゲーム');
+
+        $response->assertStatus(200);
+        $response->assertSee('ゲームする');
+        $response->assertDontSee('読書する');
+    }
+
+    public function test_can_search_Todo_by_category(): void
+    {
+        $category1=Category::create(['name'=>'テストタスク']);
+        $category2=Category::create(['name'=>'本番タスク']);
+
+        $todo1=Todo::create([
+            'content'=>'ゲームする','category_id'=>$category1->id
+        ]);
+        $todo2=Todo::create([
+            'content'=>'読書する','category_id'=>$category2->id
+        ]);
+        $response=$this->get('/todos/search?category_id=' . $category1->id);
+
+        $response->assertStatus(200);
+        $response->assertSee('ゲームする');
+        $response->assertDontSee('読書する');
+    }
+
+    public function test_can_search_Todo_by_keyword_and_category(): void
+    {
+        $category1=Category::create(['name'=>'カテゴリA']);
+        $category2=Category::create(['name'=>'カテゴリB']);
+
+        $todo1=Todo::create([
+            'content'=>'ゲームする','category_id'=>$category1->id
+        ]);
+        $todo2=Todo::create([
+            'content'=>'読書する','category_id'=>$category2->id
+        ]);
+        $response=$this->get('/todos/search?keyword=ゲーム&category_id=' . $category1->id);
+
+        $response->assertStatus(200);
+        $response->assertSee('ゲームする');
+        $response->assertDontSee('読書する');
     }
 }
